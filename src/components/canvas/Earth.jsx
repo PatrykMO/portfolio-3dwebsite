4ba14fcc19fcdfc19f00b36cccc01fe3bab13a47
Patryk Mormul <patryk.mormul@me.com>
2023-03-22 12:08:55 +0100

    Final commit

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { Suspense } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from '../Loader';
 
 const Earth = () => {
+  const earth = useGLTF('./planet/scene.gltf');
+
   return (
-    <div>Earth</div>
+    <primitive
+      object={earth.scene}
+      scale={2.5}
+      position-y={0}
+    />
   )
 }
 
-export default Earth
+const EarthCanvas = () => {
+  return (
+    <Canvas
+      shadows
+      frameloop="demand"
+      gl={{preserveDrawingBuffer: true}}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls
+          autoRotate
+          enableZoom={false}
+        />
+        <Earth />
+      </Suspense>
+    </Canvas>
+  )
+}
+
+export default EarthCanvas;
